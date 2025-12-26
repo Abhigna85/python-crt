0dcb4231f3b2bab10913472b91ca78fd59adce7f
Abhigna85 <abhignakanuma@gmail.com>
2025-12-26 14:03:32 +0530

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,16 @@
+
+triangle pattern
+
+x=int(input())
+for i in range(x):
+    for j in range(i+1):
+        print("*",end=" ")
+    print(i)
+    
+   square pattern
+    
+x=int(input())
+for i in range(x):
+    for j in range(x):
+        print("*",end=" ")
+    print(i)
